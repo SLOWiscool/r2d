e00f4c04ff6c8158f9716019585789073671008b
SLOWiscool <superslashergamez1@gmail.com>
2026-02-14 18:41:16 +0000

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(express.static("public")); // serves html
 
-const WEBHOOK_URL = "PASTE_WEBHOOK_URL_HERE";
+const WEBHOOK_URL = "https://discord.com/api/webhooks/1472301642144551056/QxYtAEY68AUjVXxc_v5KnQFvv-3FH0bykL_oTrzWjsfR8MMX_X8YnWI1PblP51ildRXO";
 
 // endpoint roblox will send to
 app.post("/roblox", async (req, res) => {
